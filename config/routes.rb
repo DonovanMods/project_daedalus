2b93a8b2b642a591db1805c7e16b5c7fce37a63a
Donovan Young <dyoung522@gmail.com>
2023-01-10 23:31:43 -0500

    Fix hover color on light screens

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :mods
+  # resources :mods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "/mods", to: "mods#index", as: "mods"
+  get "/mods/:id", to: "mods#show", as: "mod"
+
+  # get "/home", to: "home#index"
 
   # Defines the root path route ("/")
   root "home#index"
