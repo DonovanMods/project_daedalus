c60c9bb36d13e648e984255f952b742a392b5e17
Donovan Young <dyoung522@gmail.com>
2023-01-30 22:41:40 -0500

    Refactor mod and tool controllers

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 class ModsController < ApplicationController
-  before_action :fetch_mods, only: %i[index show]
+  before_action :authors, only: %i[index show]
+  before_action :mods, only: %i[index show]
 
   def index
     @mods = find_mods_by_author(sanitize(params[:author])) if params[:author].present?
 
     # If we're given a mod ID, try to find it and redirect to the mod's page
     if @mods.empty? && params[:author].present?
-      fetch_mods
-      @mod = @mods.find { |mod| mod.id == params[:author] }
-      return redirect_to mod_detail_path(author: @mod.author, slug: @mod.slug) if @mod.present?
+      @mod = mods.find { |mod| mod.id == params[:author] }
+      return redirect_to mod_detail_path(author: @mod.author_slug, slug: @mod.slug) if @mod.present?
     end
 
     # Perform a search if we have a query
@@ -30,7 +30,7 @@ class ModsController < ApplicationController
   end
 
   def show
-    @mod = @mods.find { |mod| mod.author.casecmp(params[:author])&.zero? && mod.slug.casecmp(params[:slug])&.zero? }
+    @mod = mods.find { |mod| mod.author_slug.casecmp(params[:author].parameterize)&.zero? && mod.slug.casecmp(params[:slug])&.zero? }
 
     return unless @mod.nil?
 
@@ -44,7 +44,7 @@ class ModsController < ApplicationController
   private
 
   def find_mods(query)
-    @mods.find_all do |mod|
+    mods.find_all do |mod|
       mod.name               =~ /#{query}/i ||
         mod.author           =~ /#{query}/i ||
         mod.compatibility    =~ /#{query}/i ||
@@ -54,11 +54,14 @@ class ModsController < ApplicationController
   end
 
   def find_mods_by_author(author)
-    @mods.find_all { |mod| mod.author.casecmp(author).zero? }
+    mods.find_all { |mod| mod.author_slug.casecmp(author.parameterize).zero? }
   end
 
-  def fetch_mods
+  def authors
+    @authors ||= mods.map(&:author).uniq.sort
+  end
+
+  def mods
     @mods ||= Mod.all
-    @authors = @mods.map(&:author).uniq.sort
   end
 end
--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ToolsController < ApplicationController
-  before_action :fetch_tools, only: %i[index]
+  before_action :tools, only: %i[index]
 
   def index
     @tools = find_by_author(sanitize(params[:author])) if params[:author].present?
@@ -9,12 +9,11 @@ class ToolsController < ApplicationController
 
   private
 
-  def fetch_tools
+  def tools
     @tools ||= Tool.all
-    @authors = @tools.map(&:author).uniq.sort
   end
 
   def find_by_author(author)
-    @tools.find_all { |tool| tool.author.casecmp(author).zero? }
+    @tools.find_all { |tool| tool.author_slug.casecmp(author.parameterize).zero? }
   end
 end
