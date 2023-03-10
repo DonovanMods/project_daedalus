2d0aa2be1813a4915d082d6e11c9f69751238ea5
Donovan Young <dyoung522@gmail.com>
2023-03-09 23:14:32 -0500

    Add Characters view

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,11 @@
 class CharactersController < ApplicationController
   def index
     @characters = Character.all
+
+    if turbo_frame_request?
+      render partial: "characters", locals: {characters: @characters}
+    else
+      render :index
+    end
   end
 end
