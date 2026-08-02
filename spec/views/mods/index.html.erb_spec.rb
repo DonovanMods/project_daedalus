8e95cea29063d75faa4de434b5f4ad51f4c7784b
Donovan C. Young <dyoung522@gmail.com>
2026-08-01 21:41:03 -0400

    Split option-order assertions into their own example

--- spec/views/mods/index.html.erb_spec.rb
+++ spec/views/mods/index.html.erb_spec.rb
@@ -134,6 +134,11 @@ RSpec.describe "mods/index.html.erb", type: :view do
       expect(rendered).to include(">PAK<")
       expect(rendered).to include(">ZIP<")
       expect(rendered).to include(">EXMOD(z)<")
+    end
+
+    it "renders the options in order" do
+      render
+
       expect(rendered.index(">DL Type<")).to be < rendered.index(">PAK<")
       expect(rendered.index(">PAK<")).to be < rendered.index(">ZIP<")
       expect(rendered.index(">ZIP<")).to be < rendered.index(">EXMOD(z)<")
